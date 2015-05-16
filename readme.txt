c51ba86aed29959d494a16ed65c4bd904e7dcca7
arnolem <arnolem@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-16 11:52:21 +0000

    - Initial version
    git-svn-id: https://plugins.svn.wordpress.org/wixiweb-firephp-queries/trunk@1161767 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Wixiweb FirePHP Queries ===
+Contributors: arnolem
+Donate link: https://twitter.com/arnolem
+Tags: analyse, debug, firephp, php, sql, tuning, firebug, firefox, bug, plugin, development, logging, query, queries, performance, debugging
+Requires at least: 2.0.0
+Tested up to: 4.2.2
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Use Firebug with FirePHP to analyze the SQL queries made by Wordpress. Ideal for development and avoid performance issues.
+
+== Description ==
+
+Use Firebug with **FirePHP** to **analyze the SQL queries** made by Wordpress. Ideal for development and **avoid performance issues**.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload "wixiweb-firephp-queries" folder to the /wp-content/plugins/ directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Make sure **SAVEQUERIES** is defined in your wp-config.php. Add the following line: `define('SAVEQUERIES',true);`
+1. Install Firebug : https://addons.mozilla.org/firefox/addon/firebug/
+1. Install Firephp : https://addons.mozilla.org/firefox/addon/firephp
+
+== Screenshots ==
+
+1. "wixiweb-firephp-queries" for Wordpress to view all SQL Queries in FirePHP
+
+== Changelog ==
+
+= 1.0 =
+* First version
+
+== Upgrade Notice ==
+
+
+== Frequently Asked Questions ==
+
+
+== Credits ==
+
+* [Arnaud Lemercier](http://arnaud.lemercier.me "Développeur Web Rouen")
+* [Wixiweb](http://wixiweb.fr/ "Agence développement Wordpress Rouen")
+* [FirePHP](http://firephp.org/ "Firebug Extension")
---
+++ wixiweb-firephp-queries.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Plugin Name: Wixiweb Firephp Queries
+ * Plugin URI:  http://wixiweb.fr
+ * Description: Use Firebug with FirePHP to analyze the SQL queries made by Wordpress. Ideal for development and avoid performance issues.
+ * Author:      Arnaud Lemercier
+ * Version:     1.0
+ * Author URI:  http://arnaud.lemercier.me
+ */
+include_once 'vendor/fb.php';
+
+new WixiwebFirephpQueries();
+
+class WixiwebFirephpQueries
+{
+
+    public function __construct()
+    {
+
+        add_action('init', function() {
+
+            $firephp = FirePHP::getInstance(true);
+
+            if (!current_user_can('administrator')) {
+                return;
+            }
+
+            if(!defined('SAVEQUERIES')){
+                define( 'SAVEQUERIES', true );
+                $firephp->warn('`Wixiweb FirePHP Queries` enabled SAVEQUERIES. Only SQL Queries made after `Wixiweb FirePHP Queries` is logged.');
+                $firephp->info('For best results, activate SAVEQUERIES from your wp-config.php file to see all the SQL queries');
+            }
+        }, 0);
+
+        add_action('wp', function() {
+            
+            $firephp = FirePHP::getInstance(true);
+
+            if (!current_user_can('administrator')) {
+                $firephp->error('You must be logged in as Administrator to use `Wixiweb FirePHP Queries`');
+                return;
+            }
+
+            if(!SAVEQUERIES){
+                $firephp->error('You must switch `SAVEQUERIES` to `true` from `wp-config.php` to capture SQL queries');
+                return;
+            }
+
+            global $wpdb;
+            $queries = is_array($wpdb->queries) ? $wpdb->queries : array();
+            
+            $nbQueries = count($queries);
+            $time = round(array_sum(array_column($queries, 1)), 4);
+
+            array_unshift($queries, array(
+                'Queries ('.$nbQueries.')',
+                'Time ('.$time.')',
+                'Stack'
+            ));
+
+            $firephp->table('SQL Queries ('.$nbQueries.' - '.$time.')', $queries);
+        }, 999);
+
+    }
+}
